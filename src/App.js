c30531a89c2b64c428bd6f3620e8eaa5e14204a2
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-28 19:27:14 +0600

    added footer and update website title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2022 - Developed by <a href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener no noreferrer">Ehsan</a></p>
+        </footer>
+    );
+};
+
+export default Footer;
