421697fa50c8f35a01802ded3d0635c812b31e67
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-28 17:20:20 +0600

    added quantity on cart after 1st time loading

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
+    let quantity = 0;
+    for (const player of cart) {
+        quantity += player.quantity;
+    }
     return (
         <div className='cart'>
             <h1>Booked Players</h1>
-            <h3>Selected Players: {cart.length}</h3>
+            <h3>Selected Players: {quantity}</h3>
             <ol>
                 {
                     cart.map(player => <li>{player.strPlayer}</li>)
